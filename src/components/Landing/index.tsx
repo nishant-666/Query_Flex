139c3f42c55ce021bf45f928195054dec3aa9206
Nishant Kumar <nishants440@gmail.com>
2023-08-21 08:40:17 +0530

    Text Area Added

--- src/components/Landing/index.tsx
+++ src/components/Landing/index.tsx
@@ -1,12 +1,19 @@
 import React from "react";
 import styles from "./Landing.module.scss";
+import { TfiReload } from "react-icons/tfi";
 
 export default function LandingComponent() {
   return (
-    <section
-      className={`prose flex md:container lg:prose-xl md:mx-auto ${styles.landingMain}`}
-    >
-      <h1 className={`justify-center ${styles.header}`}>Landing Page</h1>
+    <section className={`${styles.landingMain}`}>
+      <textarea
+        rows={10}
+        className={`textarea textarea-accent ${styles.textArea}`}
+        placeholder="Enter your Query!"
+      ></textarea>
+      <section className={styles.btnContainer}>
+        <button className={`btn btn-accent ${styles.submitBtn}`}>Submit</button>
+        <TfiReload className={styles.reload} size={45} />
+      </section>
     </section>
   );
 }
