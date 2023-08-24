72ac45310a5f12ee15fd6ee75ebe37592d90c014
Nishant Kumar <nishants440@gmail.com>
2023-08-25 01:07:26 +0530

    FInal Changes

--- src/hooks/useCheckVerification.ts
+++ src/hooks/useCheckVerification.ts
@@ -5,12 +5,12 @@ export default function useCheckVerification() {
   const [verificationLoading, setVerificationLoading] = useState(true);
   const [verificationChecked, setVerificationChecked] = useState(false);
   useEffect(() => {
+    setVerificationLoading(false);
     const checkForVerifiedInterval = setInterval(() => {
       auth.currentUser?.reload().then(() => {
         if (auth.currentUser?.emailVerified) {
           clearInterval(checkForVerifiedInterval);
           setVerificationChecked(true);
-          setVerificationLoading(false);
         }
       });
     }, 1000);
